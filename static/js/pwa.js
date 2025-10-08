58676f3b46a0d135c829b87535b94a4c9feae827
maxandul <ulrich.andreas90@gmail.com>
2025-10-08 16:51:12 +0200

    Revert "Bump to v1.3.11: VAPID Key Debug-Logging"
    This reverts commit d3f7f74b11a6d66eb5192441f7e5882cb30839d1.

--- static/js/pwa.js
+++ static/js/pwa.js
@@ -862,7 +862,7 @@ class PWA {
         // App-Version anzeigen
         const versionSpan = document.getElementById('app-version');
         if (versionSpan) {
-            versionSpan.textContent = '1.3.11';
+            versionSpan.textContent = '1.3.10';
         }
 
         // Installationsstatus prüfen
