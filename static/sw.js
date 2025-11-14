a70f77a734c1a3d744ae3c91734131a361b02be8
maxandul <ulrich.andreas90@gmail.com>
2025-11-14 17:54:40 +0100

    Remove Gesamtbestellung card, move copy button to page-actions

--- static/sw.js
+++ static/sw.js
@@ -3,9 +3,9 @@
  * Verbesserte Offline-Funktionalität und Update-Management
  */
 
-const CACHE_NAME = 'gourmen-v1.4.8';
-const STATIC_CACHE = 'gourmen-static-v1.4.8';
-const DYNAMIC_CACHE = 'gourmen-dynamic-v1.4.8';
+const CACHE_NAME = 'gourmen-v1.4.9';
+const STATIC_CACHE = 'gourmen-static-v1.4.9';
+const DYNAMIC_CACHE = 'gourmen-dynamic-v1.4.9';
 
 // Assets die gecacht werden sollen (nur wirklich statische Dateien!)
 // JavaScript-Dateien NICHT hier, damit Updates sofort ankommen
